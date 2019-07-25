4282b5d105a6ef8453ffdde0dc4c0caf4c3fc7f6
grogdotcom <g.herman27@gmail.com>
2019-07-25 16:17:04 -0400

    removed commented out code

--- src/main/scala/soc/game/GameState.scala
+++ src/main/scala/soc/game/GameState.scala
@@ -347,31 +347,5 @@ case class GameState(board: CatanBoard,
     },
     turnState = TurnState()
   )
-
-
-  case class StateWithProbability(state: GameState, prob: Double = 1.0)
-  def getPossibleStatesForMove(playerId: Int, move: CatanMove): Iterator[StateWithProbability] = move match {
-
-    case InitialPlacementMove(first, settlement, road) => Iterator(StateWithProbability(initialPlacement(playerId, first, settlement, road)))
-    case BuildRoad(edge) => Iterator(StateWithProbability(buildRoad(playerId, edge)))
-    case BuildSettlement(vertex) => Iterator(StateWithProbability(buildSettlement(playerId, vertex)))
-    case BuildCity(vertex) => Iterator(StateWithProbability(buildCity(playerId, vertex)))
-    case PortTrade(give, get) => Iterator(StateWithProbability(portTrade(playerId, give, get)))
-
-    case YearOfPlentyMove(res1, res2) => Iterator(StateWithProbability(playYearOfPlenty(playerId, res1, res2)))
-    case RoadBuilderMove(road1, road2) => Iterator(StateWithProbability(playRoadBuilder(playerId, road1, road2)))
-    case EndTurnMove => Iterator(StateWithProbability(endTurn(playerId)))
-
-    case RollDiceMove => (2 to 12).toIterator.flatMap {
-      case r if r != 7 =>
-        val roll = Roll(r)
-        Iterator(StateWithProbability(rollDice(playerId, roll), roll.prob))
-      case 7 =>
-        CatanPossibleMoves(this, playerId).getPossibleDiscards()
-
-    }
-
-  }
-
 }
 
