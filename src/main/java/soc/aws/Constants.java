761ad60f0121892a34950bcb90211c9a08c17838
Ilan Shenar <ishenar@amazon.com>
2019-08-24 10:49:58 -0400

    Built tests against AWS and Implemented Gets

--- src/main/java/soc/aws/Constants.java
+++ src/main/java/soc/aws/Constants.java
@@ -6,7 +6,10 @@ public class Constants
 
     public static final String BOARD_BUCKET = "catan-boards";
 
-    public static final String ACCESS_KEY = "HIDDEN";
-    public static final String SECRET_KEY = "HIDDEN";
+    public static final String MOVESET_S3KEY = "MoveSet_S3Key";
+    public static final String BOARD_S3KEY = "Board_S3Key";
+
+    public static final String ACCESS_KEY = "AKIAT5IG2CSMNRDX6752";
+    public static final String SECRET_KEY = "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";
 
 }
